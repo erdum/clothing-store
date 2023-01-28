d2e3288b51511105999ddeaf8b60b121d1802fd3
Erdum <erdumadnan@gmail.com>
2023-01-28 17:54:00 +0500

    Update product relationships defined

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,44 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\SubCategory;
+use App\Models\ProductSize;
+use App\Models\ProductColor;
+use App\Models\ProductImage;
+use App\Models\OrderItem;
+use App\Models\Cart;
+
 class Product extends Model
 {
     use HasFactory;
+
+    public function sub_category()
+    {
+        return $this->belongsTo(SubCategory::class);
+    }
+
+    public function sizes()
+    {
+        return $this->hasMany(ProductSize::class);
+    }
+
+    public function colors()
+    {
+        return $this->hasMany(ProductColor::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function in_carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
 }
