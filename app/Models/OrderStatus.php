785d7b54df0475fa7a4c25624b8cfec2cd430aa7
Erdum <erdumadnan@gmail.com>
2023-01-29 16:01:43 +0500

    Update order status relationship defined

--- app/Models/OrderStatus.php
+++ app/Models/OrderStatus.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Order;
+
 class OrderStatus extends Model
 {
     use HasFactory;
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
