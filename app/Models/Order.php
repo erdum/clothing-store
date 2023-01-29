3453af03448da06fab6e2034f280f9ebc593999b
Erdum <erdumadnan@gmail.com>
2023-01-29 15:58:18 +0500

    Update order relationships defined

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,7 +5,32 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\User;
+use App\Models\OrderStatus;
+use App\Models\ShippingAddress;
+use App\Models\OrderItem;
+
 class Order extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function status()
+    {
+        return $this->belongsTo(OrderStatus::class);
+    }
+
+    public function shipping_address()
+    {
+        return $this->belongsTo(ShippingAddress::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class)->product();
+    }
 }
