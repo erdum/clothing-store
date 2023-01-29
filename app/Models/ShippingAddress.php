ebf22f16b25b5176685c8df2afa7e39ce4fd5966
Erdum <erdumadnan@gmail.com>
2023-01-29 16:05:41 +0500

    Update shipping address relationship defined

--- app/Models/ShippingAddress.php
+++ app/Models/ShippingAddress.php
@@ -5,7 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\Order;
+
 class ShippingAddress extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
 }
