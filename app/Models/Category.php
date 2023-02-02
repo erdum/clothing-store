2c10f3df75cab12d4ee9c1ee2bd1d1b55e8aab23
Erdum <erdumadnan@gmail.com>
2023-02-03 00:09:11 +0500

    Update category relationship defined

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\SubCategory;
+
 class Category extends Model
 {
     use HasFactory;
+
+    public function sub()
+    {
+        return $this->hasMany(SubCategory::class);
+    }
 }
