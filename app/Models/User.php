3a7ad61ccc7a66edf362aaf8ca6f2ef1088aee72
Erdum <erdumadnan@gmail.com>
2023-02-02 23:20:49 +0500

    Update naming corrected

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function in_cart_item()
+    public function in_cart_items()
     {
         return $this->hasMany(Cart::class)->product();
     }
