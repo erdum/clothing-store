fbc4f3ed38e9b9adbd09e0be44895156678ab07c
Erdum <erdumadnan@gmail.com>
2023-02-05 22:33:36 +0500

    Fix syntax error

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,15 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class CheckoutController extends Controller
 {
     public function index()
     {
-        return 'Checkout';
+        return View::make('checkout.index', [
+
+        ]);
     }
 
-    pulic function checkout()
+    public function checkout()
     {
         return 'Checkout';
     }
