4c966119eb2404e6375eb829f2252935af8a93b6
Erdum <erdumadnan@gmail.com>
2023-02-06 17:17:39 +0500

    Update Product controller implemented

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,17 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
+use App\Models\Product;
+
 class ProductController extends Controller
 {
     public function index($id)
     {
-        return View::make('product.product', ['product' => ((object) [
-            'name' => 'Jeans',
-            'unit_price' => 30,
-            'description' => 'lorem ipsum doler sit asdf kasdfa kjlda io lsadf.',
-            'quantity' => 21,
-            'images' => [],
-            'sizes' => [],
-        ]), 'sub_category_name' => 'Men']);
+        $product = Product::find($id);
+
+        return View::make('product.product', [
+            'product' => $product,
+            'sub_category_name' => $product->sub_category->name,
+        ]);
     }
 }
