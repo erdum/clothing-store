88467aa39b27bdaa96a2ed420423092fab661345
Erdum <erdumadnan@gmail.com>
2023-02-11 22:38:07 +0500

    Update orders page implemented

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class OrderController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return 'Orders';
+        $user = $request->user();
+
+        if ($request->id) {
+            $order = $user->orders->find($request->id);
+
+            return View::make('order.order', []);
+        }
+
+        $orders = $user->orders;
+        
+        return View::make('order.index', ['orders' => $orders]);
     }
 }
---
+++ resources/views/order/index.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.master')
+
+@section('title', 'My Orders')
+
+@section('stylesheets')
+    <link rel="stylesheet" href="{{ asset('css/orders.css') }}">
+@endsection
+
+@section('content')
+    <div class="container">
+        <h1>Orders</h1>
+    </div>
+    <div class="heck">
+        <div class="title-container">
+            <span id="your-orders" class="extra active">Your Order's</span>
+        </div>
+        <div class="orders prev-order">
+            <div class="titles">
+                <div class="order-num title">
+                    <h4>Order Number</h4>
+                </div>
+                <div class="status title">
+                    <h4>Status</h4>
+                </div>
+                <div class="customer title">
+                    <h4>Name</h4>
+                </div>
+                <div class="date title">
+                    <h4>Date</h4>
+                </div>
+                <div class="total title">
+                    <h4>Total</h4>
+                </div>
+            </div>
+            @foreach ($orders as $order)
+                <div class="order-det">
+                    <div class="order-num det">
+                        <a href="{{ route('order', ['id' => $order->id]) }}">
+                            <p><span>#{{ $order->id }}</span></p>
+                        </a>
+                    </div>
+                    <div class="status det">
+                        <p>{{ $order->status }}</p>
+                    </div>
+                    <div class="customer det">
+                        <p>{{ $order->user->name }}</p>
+                        <p><span>{{ $order->user->email }}</span></p>
+                    </div>
+                    <div class="date det">
+                        <p>{{ $order->created_at }}</p>
+                    </div>
+                    <div class="total det">
+                        <h4>Rs: <span>{{ $order->total }}</span></h4>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
