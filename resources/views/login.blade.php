07028d1447f1d6c4344708254da0be78d5b7563c
Erdum <erdumadnan@gmail.com>
2023-03-07 23:14:18 +0500

    Update login view replaced

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,35 +1,74 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="h-full bg-gray-50" lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <!-- Font Awesme Icon Link -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- FavIcon Link -->
     <link rel="shortcut icon" href="{{ asset('assets/site/images/favicon/favicon-32x32.png') }}" type="image/x-icon">
     <title>Login - Signup | Apparel UB365Inn</title>
-    <link rel="stylesheet" href="{{ asset('assets/site/css/login.css') }}">
+    <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
 </head>
-
-<body>
-    <a href="{{ route('home') }}" class="back"><i class="fa-solid fa-arrow-left"></i></a>
-
-    <div class="sign-in-form">
-        <img src="{{ asset('assets/site/images/icon3.png') }}">
-        <h1>Log In</h1>
-        <form>
-            <input name="email" type="email" class="input-box" placeholder="Your Email">
-            <input name="password" type="password" class="input-box" placeholder="Your Password">
-            <button type="button" class="form-btn login-btn">Log In</button>
-            <hr>
-            <p class="or">OR</p>
-            <a href="{{ route('third_party_login', ['provider_name' => 'google']) }}" class="form-btn"><i class="fa-brands fa-google"></i> Login with Google</a>
-            <a href="{{ route('third_party_login', ['provider_name' => 'facebook']) }}" class="form-btn"><i class="fa-brands fa-facebook"></i> Login with Facebook</a>
-            
-        </form>
+  <body class="h-full">
+<div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+  <div class="w-full max-w-md space-y-8">
+    <div>
+      <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
+      <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+      <!-- <p class="mt-2 text-center text-sm text-gray-600">
+        Or
+        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
+      </p> -->
     </div>
+    <form class="mt-8 space-y-6" action="{{ route('login_post') }}" method="POST">
+        @csrf
+      <input type="hidden" name="remember" value="true">
+      <div class="-space-y-px rounded-md shadow-sm">
+        <div>
+          <label for="email-address" class="sr-only">Email address</label>
+          <input id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Email address">
+        </div>
+        <div>
+          <label for="password" class="sr-only">Password</label>
+          <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Password">
+        </div>
+      </div>
+
+      <div class="flex items-center justify-between">
+        <div class="flex items-center">
+          <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600">
+          <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
+        </div>
+
+        <div class="text-sm">
+          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+        </div>
+      </div>
+
+      <div>
+        <button type="submit" class="group relative flex w-full justify-center rounded-md bg-indigo-600 py-3 md:py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+            <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+              <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+            </svg>
+          </span>
+          Sign in / Sign up
+        </button>
+        <span class="block h-px w-full bg-gray-300 my-6"></span>
+        <a href="{{ route('third_party_login', ['provider_name' => 'google']) }}" class="border border-gray-300 my-3 group relative flex w-full justify-center rounded-md bg-white py-3 md:py-2 px-3 text-sm font-semibold text-gray-400 shadow-sm hover:bg-gray-400 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            <img class="w-5 mr-2" src="{{ asset('assets/site/images/google.svg') }}">
+            Google
+        </a>
+        <a href="{{ route('third_party_login', ['provider_name' => 'facebook']) }}" class="border border-gray-300 my-3 group relative flex w-full justify-center rounded-md bg-white py-3 md:py-2 px-3 text-sm font-semibold text-gray-400 shadow-sm hover:bg-gray-400 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            <img class="w-5 mr-2" src="{{ asset('assets/site/images/facebook.svg') }}">
+            Facebook
+        </a>
+      </div>
+    </form>
+  </div>
+</div>
+
 </body>
 
 </html>
