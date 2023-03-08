d50705784e9a78946bf7e7ce36fe3422a99e94f5
Erdum <erdumadnan@gmail.com>
2023-03-08 12:55:55 +0500

    Update sidebar cart added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -203,13 +203,13 @@
                             </div> -->
                             <!-- Cart -->
                             <div class="ml-4 flow-root lg:ml-6">
-                                <a href="#" class="group -m-2 flex items-center p-2">
+                                <button id="sidebar-cart-btn" class="group -m-2 flex items-center p-2 cursor-pointer">
                                     <svg class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                                     </svg>
                                     <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
                                     <span class="sr-only">items in cart, view bag</span>
-                                </a>
+                                </button>
                             </div>
                         </div>
                     </div>
@@ -217,6 +217,133 @@
             </nav>
         </header>
     </div>
+
+    <div class="relative z-10" aria-labelledby="slide-over-title" role="dialog" aria-modal="true">
+  <!--
+    Background backdrop, show/hide based on slide-over state.
+
+    Entering: "ease-in-out duration-500"
+      From: "opacity-0"
+      To: "opacity-100"
+    Leaving: "ease-in-out duration-500"
+      From: "opacity-100"
+      To: "opacity-0"
+  -->
+  <div id="sidebar-cart-overlay" class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity hidden"></div>
+
+  <div class="fixed top-0 bottom-0 overflow-hidden">
+    <div class="absolute inset-0 overflow-hidden">
+      <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+        <!--
+          Slide-over panel, show/hide based on slide-over state.
+
+          Entering: "transform transition ease-in-out duration-500 sm:duration-700"
+            From: "translate-x-full"
+            To: "translate-x-0"
+          Leaving: "transform transition ease-in-out duration-500 sm:duration-700"
+            From: "translate-x-0"
+            To: "translate-x-full"
+        -->
+        <div id="sidebar-cart" class="pointer-events-auto w-screen max-w-md translate-x-full transform transition ease-in-out duration-500">
+          <div class="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+            <div class="flex-1 overflow-y-auto py-6 px-4 sm:px-6">
+              <div class="flex items-start justify-between">
+                <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Shopping cart</h2>
+                <div class="ml-3 flex h-7 items-center">
+                  <button id="sidebar-cart-close-btn" type="button" class="-m-2 p-2 text-gray-400 hover:text-gray-500">
+                    <span class="sr-only">Close panel</span>
+                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+
+              <div class="mt-8">
+                <div class="flow-root">
+                  <ul role="list" class="-my-6 divide-y divide-gray-200">
+                    <li class="flex py-6">
+                      <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                        <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg" alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt." class="h-full w-full object-cover object-center">
+                      </div>
+
+                      <div class="ml-4 flex flex-1 flex-col">
+                        <div>
+                          <div class="flex justify-between text-base font-medium text-gray-900">
+                            <h3>
+                              <a href="#">Throwback Hip Bag</a>
+                            </h3>
+                            <p class="ml-4">$90.00</p>
+                          </div>
+                          <p class="mt-1 text-sm text-gray-500">Salmon</p>
+                        </div>
+                        <div class="flex flex-1 items-end justify-between text-sm">
+                          <p class="text-gray-500">Qty 1</p>
+
+                          <div class="flex">
+                            <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+
+                    <li class="flex py-6">
+                      <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                        <img src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg" alt="Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch." class="h-full w-full object-cover object-center">
+                      </div>
+
+                      <div class="ml-4 flex flex-1 flex-col">
+                        <div>
+                          <div class="flex justify-between text-base font-medium text-gray-900">
+                            <h3>
+                              <a href="#">Medium Stuff Satchel</a>
+                            </h3>
+                            <p class="ml-4">$32.00</p>
+                          </div>
+                          <p class="mt-1 text-sm text-gray-500">Blue</p>
+                        </div>
+                        <div class="flex flex-1 items-end justify-between text-sm">
+                          <p class="text-gray-500">Qty 1</p>
+
+                          <div class="flex">
+                            <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">Remove</button>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+
+                    <!-- More products... -->
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div class="border-t border-gray-200 py-6 px-4 sm:px-6">
+              <div class="flex justify-between text-base font-medium text-gray-900">
+                <p>Subtotal</p>
+                <p>$262.00</p>
+              </div>
+              <p class="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+              <div class="mt-6">
+                <a href="#" class="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Checkout</a>
+              </div>
+              <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
+                <p>
+                  or
+                  <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500">
+                    Continue Shopping
+                    <span aria-hidden="true"> &rarr;</span>
+                  </button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 
     <script>
         const openMobileMenu = () => {
@@ -235,6 +362,22 @@
             mobileSideMenu.classList.remove('translate-x-0');
         };
 
+        const openSidebarCart = () => {
+            const sidebarCart = document.getElementById('sidebar-cart');
+            const sidebarCartOverlay = document.getElementById('sidebar-cart-overlay');
+            sidebarCart.classList.remove('translate-x-full');
+            sidebarCartOverlay.classList.remove('hidden');
+            sidebarCart.classList.add('translate-x-0');
+        };
+
+        const closeSidebarCart = () => {
+            const sidebarCart = document.getElementById('sidebar-cart');
+            const sidebarCartOverlay = document.getElementById('sidebar-cart-overlay');
+            sidebarCart.classList.remove('translate-x-0');
+            sidebarCart.classList.add('translate-x-full');
+            sidebarCartOverlay.classList.add('hidden');
+        };
+
         const handleTabClick = ({ target: { id } }) => {
             document.querySelectorAll('.mobile-sidebar-panel').forEach(panel => panel.classList.add('hidden'));
             const key = id.split('-')[0];
@@ -244,6 +387,10 @@
         document.getElementById('mobile-menu-btn').addEventListener('click', openMobileMenu);
         document.getElementById('mobile-sidebar-menu-overlay').addEventListener('click', closeMobileMenu);
         document.getElementById('mobile-menu-btn-close').addEventListener('click', closeMobileMenu);
+
+        document.getElementById('sidebar-cart-btn').addEventListener('click', openSidebarCart);
+        document.getElementById('sidebar-cart-close-btn').addEventListener('click', closeSidebarCart);
+        document.getElementById('sidebar-cart-overlay').addEventListener('click', closeSidebarCart);
 
         document.getElementById('mobile-sidebar-tabs').addEventListener('click', handleTabClick);
     </script>
