0ca39bad8d3ee7b9746c4d4d2e290f0851a2f765
Erdum <erdumadnan@gmail.com>
2023-03-08 17:41:09 +0500

    Update category controller and route removed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,8 +7,26 @@ use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index($category = null, $sub_category = null)
     {
+        // if (!empty($name) && !empty($sub_name)) {
+        //     $sub_category = Category::find($name)->sub->find($sub_name);
+
+        //     return View::make('product.listing', [
+        //         'sub_category' => $sub_category,
+        //         'products' => Product::all(),
+        //     ]);
+        // }
+
+        // if (!empty($name)) {
+        //     $category = Category::find($name);
+
+        //     return View::make('category.sub-categories', [
+        //         'category' => $category,
+        //         'sub_categories' => $category->sub,
+        //     ]);
+        // }
+        
         return View::make('layouts.master');
     }
 
