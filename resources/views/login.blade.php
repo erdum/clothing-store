139bf12e7e4f86253e0fa6f8fb0585656963045e
Erdum <erdumadnan@gmail.com>
2023-03-08 14:45:09 +0500

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -16,10 +16,10 @@
     <div>
       <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
       <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-      <!-- <p class="mt-2 text-center text-sm text-gray-600">
+      <p class="mt-2 text-center text-sm text-gray-600">
         Or
-        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
-      </p> -->
+        <a href="signup" class="font-medium text-indigo-600 hover:text-indigo-500">create new account</a>
+      </p>
     </div>
     <form class="mt-8 space-y-6" action="{{ route('login_post') }}" method="POST">
         @csrf
@@ -47,20 +47,20 @@
       </div>
 
       <div>
-        <button type="submit" class="group relative flex w-full justify-center rounded-md bg-indigo-600 py-3 md:py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <button type="submit" class="group relative flex w-full justify-center rounded-md bg-indigo-600 py-3 md:py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-colors````">
           <span class="absolute inset-y-0 left-0 flex items-center pl-3">
             <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
             </svg>
           </span>
-          Sign in / Sign up
+          Sign in
         </button>
         <span class="block h-px w-full bg-gray-300 my-6"></span>
-        <a href="{{ route('third_party_login', ['provider_name' => 'google']) }}" class="border border-gray-300 my-3 group relative flex w-full justify-center rounded-md bg-white py-3 md:py-2 px-3 text-sm font-semibold text-gray-400 shadow-sm hover:bg-gray-400 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <a href="{{ route('third_party_login', ['provider_name' => 'google']) }}" class="border border-gray-300 my-3 group relative flex w-full justify-center rounded-md bg-white py-3 md:py-2 px-3 text-sm font-semibold text-gray-400 shadow-sm hover:bg-gray-400 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-colors````">
             <img class="w-5 mr-2" src="{{ asset('assets/site/images/google.svg') }}">
             Google
         </a>
-        <a href="{{ route('third_party_login', ['provider_name' => 'facebook']) }}" class="border border-gray-300 my-3 group relative flex w-full justify-center rounded-md bg-white py-3 md:py-2 px-3 text-sm font-semibold text-gray-400 shadow-sm hover:bg-gray-400 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+        <a href="{{ route('third_party_login', ['provider_name' => 'facebook']) }}" class="border border-gray-300 my-3 group relative flex w-full justify-center rounded-md bg-white py-3 md:py-2 px-3 text-sm font-semibold text-gray-400 shadow-sm hover:bg-gray-400 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-colors````">
             <img class="w-5 mr-2" src="{{ asset('assets/site/images/facebook.svg') }}">
             Facebook
         </a>
