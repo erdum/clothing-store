61de97694d27be8bfcd3c7e1b67d6b1e860eacd0
Erdum <erdumadnan@gmail.com>
2023-03-08 17:14:01 +0500

    Update master view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -89,13 +89,23 @@
                                 <a href="{{ route('contact-us') }}" class="-m-2 block p-2 font-medium text-gray-900">Contact Us</a>
                             </div>
                         </div>
+                        <div class="border-t border-gray-200 py-6 px-4">
+                            <a class="-m-2 flex items-center p-2">
+                                <img src="{{ asset('assets/site/images/user.jpg') }}" alt="Pakistan Flag" class="block h-auto w-8 flex-shrink-0 rounded-full">
+                                <span class="ml-3 block text-base font-medium text-gray-900">Hamza</span>
+                            </a>
+                        </div>
                         <div class="space-y-6 border-t border-gray-200 py-6 px-4">
                             <div class="flow-root">
+                                @if (Auth::check())
+                                <a href="{{ route('logout') }}" class="-m-2 block p-2 font-medium text-gray-900">Logout</a>
+                                @else
                                 <a href="{{ route('login') }}" class="-m-2 block p-2 font-medium text-gray-900">Sign in</a>
+                                @endif
                             </div>
                         </div>
                         <div class="border-t border-gray-200 py-6 px-4">
-                            <a href="#" class="-m-2 flex items-center p-2">
+                            <a class="-m-2 flex items-center p-2">
                                 <img src="{{ asset('assets/site/images/pakistan.png') }}" alt="Pakistan Flag" class="block h-auto w-8 flex-shrink-0">
                                 <span class="ml-3 block text-base font-medium text-gray-900">PKR</span>
                                 <span class="sr-only">, change currency</span>
@@ -187,7 +197,7 @@
                                     <span class="h-6 w-px bg-gray-200 block" aria-hidden="true"></span>
                                 </div>
                                 <div class="hidden lg:ml-6 lg:flex">
-                                    <a href="#" class="flex items-center text-gray-700 hover:text-gray-800">
+                                    <a class="flex items-center text-gray-700 hover:text-gray-800">
                                         <img src="{{ asset('assets/site/images/pakistan.png') }}" alt="Pakistan Flag" class="block h-auto w-8 flex-shrink-0">
                                         <span class="ml-3 block text-sm font-medium">PKR</span>
                                         <span class="sr-only">, change currency</span>
@@ -203,8 +213,16 @@
                                 </a>
                             </div> -->
                                 <!-- Cart -->
+                                @if (Auth::check())
+                                <div class="hidden lg:ml-6 lg:flex">
+                                    <a href="{{ route('order') }}" class="flex items-center text-gray-700 hover:text-gray-800">
+                                        <img src="{{ Auth::user()->avatar ?? asset('assets/site/images/user.jpg') }}" alt="Pakistan Flag" class="block h-auto w-8 flex-shrink-0 rounded-full">
+                                        <span class="ml-3 block text-sm font-medium">{{ Auth::user()->name }}</span>
+                                    </a>
+                                </div>
+                                @endif
                                 <div class="ml-4 flow-root lg:ml-6">
-                                    <button id="sidebar-cart-btn" class="group -m-2 flex items-center p-2 cursor-pointer">
+                                    <a id="sidebar-cart-btn" class="group -m-2 flex items-center p-2 cursor-pointer">
                                         <svg class="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
                                         </svg>
@@ -214,16 +232,8 @@
                                         <span class="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
                                         @endif
                                         <span class="sr-only">items in cart, view bag</span>
-                                    </button>
-                                </div>
-                                @if (Auth::check())
-                                <div class="hidden lg:ml-6 lg:flex">
-                                    <a href="#" class="flex items-center text-gray-700 hover:text-gray-800">
-                                        <img src="{{ asset('assets/site/images/user.jpg') }}" alt="Pakistan Flag" class="block h-auto w-8 flex-shrink-0 rounded-full">
-                                        <span class="ml-3 block text-sm font-medium">{{ Auth::user()->name }}</span>
                                     </a>
                                 </div>
-                                @endif
                             </div>
                         </div>
                     </div>
