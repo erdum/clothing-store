b745db19cbf6df752b42f049635c05e55e641eb5
Erdum <erdumadnan@gmail.com>
2023-03-08 14:49:33 +0500

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,7 +18,7 @@
       <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
       <p class="mt-2 text-center text-sm text-gray-600">
         Or
-        <a href="signup" class="font-medium text-indigo-600 hover:text-indigo-500">create new account</a>
+        <a href="{{ route('signup') }}" class="font-medium text-indigo-600 hover:text-indigo-500">create new account</a>
       </p>
     </div>
     <form class="mt-8 space-y-6" action="{{ route('login_post') }}" method="POST">
