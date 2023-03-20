3b5abd192f9bcb5245253bc93c34b7a3299d8edf
Erdum <erdumadnan@gmail.com>
2023-03-20 17:49:59 +0500

    Update site model added

---
+++ database/migrations/2023_03_17_181958_create_sites_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sites', function (Blueprint $table) {
+            $table->id();
+            $table->string('promo_discount_text');
+            $table->integer('promo_discount');
+            $table->integer('delivery_charges');
+            $table->integer('tax_charges');
+            $table->integer('currency_multiplier');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sites');
+    }
+};
