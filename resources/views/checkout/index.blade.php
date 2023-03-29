6ce45d5a3e6c7719974ee64e995ae10acff020ad
Erdum <erdumadnan@gmail.com>
2023-03-29 09:19:36 +0500

    Update index.blade.php

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -24,12 +24,6 @@
                                 <input required value="{{ $user->shipping_address->address ?? '' }}" type="text" name="address" id="address" autocomplete="street-address" class="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                             </div>
                         </div>
-                        <div class="sm:col-span-2">
-                            <label for="apartment" class="block text-sm font-medium text-gray-700">Apartment, suite, etc.</label>
-                            <div class="mt-1">
-                                <input type="text" name="apartment" id="apartment" class="block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                            </div>
-                        </div>
                         <div>
                             <label for="city" class="block text-sm font-medium text-gray-700">City *</label>
                             <div class="mt-1">
@@ -125,15 +119,15 @@
                         <legend class="sr-only">Payment type</legend>
                         <div id="payment-method-wrapper" class="space-y-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-10">
                             <div class="flex items-center cursor-pointer">
-                                <input checked value="Cash On Delivery" id="cod" name="payment_method" type="radio" class="focus:ring-indigo-500 cursor-pointer h-4 w-4 text-indigo-600 border-gray-300">
+                                <input checked value="cash_on_delivery" id="cod" name="payment_method" type="radio" class="focus:ring-indigo-500 cursor-pointer h-4 w-4 text-indigo-600 border-gray-300">
                                 <label for="credit-card" class="ml-3 block text-sm font-medium text-gray-700"> Cash On Delivery </label>
                             </div>
                             <div class="flex items-center cursor-pointer">
-                                <input value="Credit Card" id="credit-card" name="payment_method" type="radio" class="focus:ring-indigo-500 cursor-pointer h-4 w-4 text-indigo-600 border-gray-300">
+                                <input value="credit_card" id="credit-card" name="payment_method" type="radio" class="focus:ring-indigo-500 cursor-pointer h-4 w-4 text-indigo-600 border-gray-300">
                                 <label for="credit-card" class="ml-3 block text-sm font-medium text-gray-700"> Credit card </label>
                             </div>
                             <div class="flex items-center">
-                                <input value="Bank Transfer" id="bank-transfer" name="payment_method" type="radio" class="focus:ring-indigo-500 cursor-pointer h-4 w-4 text-indigo-600 border-gray-300">
+                                <input value="bank_transfer" id="bank-transfer" name="payment_method" type="radio" class="focus:ring-indigo-500 cursor-pointer h-4 w-4 text-indigo-600 border-gray-300">
                                 <label for="etransfer" class="ml-3 block text-sm font-medium text-gray-700"> Bank Transfer </label>
                             </div>
                         </div>
