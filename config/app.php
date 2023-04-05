ba70feaf51d9ea41cd3b3fa71135497fe7aab9a2
Erdum <erdumadnan@gmail.com>
2023-04-05 09:13:23 +0500

    Update package modified & removed from dependencies

--- config/app.php
+++ config/app.php
@@ -185,6 +185,8 @@ return [
         /*
          * Package Service Providers...
          */
+        EasyPanel\EasyPanelServiceProvider::class,
+        EasyBlade\EasyBladeServiceProvider::class,
 
         /*
          * Application Service Providers...
