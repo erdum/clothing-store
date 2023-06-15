bf9e7dd764926e98cbcd8b66b086203cf032eb78
Erdum <erdumadnan@gmail.com>
2023-06-15 14:41:12 +0500

    Update helper introduced

---
+++ app/helpers.php
@@ -0,0 +1,19 @@
+<?php
+
+function save_base64_to_webp($images)
+{
+    $links = [];
+    foreach ($images as $image) {
+        $data_string = explode(',', $image)[1];
+        $data = base64_decode($data_string);
+        $decoded_image = imagecreatefromstring($data);
+        $link = Str::random(15) . '.webp';
+
+        if (!file_exists(storage_path('app/products-images'))) {
+            mkdir(storage_path('app/products-images'));
+        }
+        imagewebp($decoded_image, storage_path('app/products-images/' . $link), 75);
+        array_push($links, $link);
+    }
+    return $links;
+}
