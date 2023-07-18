a3e41e3e8946fab505777683bba3d957ce41455f
Erdum <erdumadnan@gmail.com>
2023-07-19 00:29:48 +0500

    Update order actions

--- resources/views/admin-panel/orders/index.blade.php
+++ resources/views/admin-panel/orders/index.blade.php
@@ -17,12 +17,9 @@
           <thead class="bg-gray-50">
             <tr>
               <th scope="col" class="relative px-6 py-3">
-                <span class="sr-only">View</span>
+                <span class="sr-only">Manage</span>
               </th>
-              <th scope="col" class="relative px-6 py-3">
-                <span class="sr-only">Edit</span>
-              </th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">ID</th>
+              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Order ID</th>
               <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Customer Name</th>
               <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
               <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Shipping Address</th>
@@ -43,10 +40,7 @@
             @foreach ($orders as $order)
             <tr>
               <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <a href="{{ route('view-order', ['order_id' => $order->id]) }}" class="text-indigo-600 hover:text-indigo-900">View</a>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <a href="{{ route('edit-order', ['order_id' => $order->id]) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                <a href="{{ route('edit-order', ['order_id' => $order->id]) }}" class="text-indigo-600 hover:text-indigo-900">Manage</a>
               </td>
               <td class="px-6 py-4 whitespace-nowrap w-40">
                 <div class="text-sm text-gray-900">{{ $order->id }}</div>
