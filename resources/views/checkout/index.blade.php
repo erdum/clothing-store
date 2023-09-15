6e862ecccfd4697ede02505eadb2ae59729b7f0d
Erdum <erdumadnan@gmail.com>
2023-09-15 17:41:30 +0500

    Update js replaced with alpinejs

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -6,7 +6,7 @@
 <div class="bg-gray-50">
     <div class="max-w-2xl mx-auto pt-16 pb-24 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
         <h2 class="sr-only">Checkout</h2>
-        <form action="{{ route('post_checkout') }}" method="POST" class="lg:grid lg:grid-cols-2 lg:gap-x-12 xl:gap-x-16">
+        <form x-data="{ shippingMethod: '{{ $shipping_methods[0]->name }}' }" action="{{ route('post_checkout') }}" method="POST" class="lg:grid lg:grid-cols-2 lg:gap-x-12 xl:gap-x-16">
             @csrf
             <div>
                 <div>
@@ -69,55 +69,46 @@
                     <fieldset>
                         <legend class="text-lg font-medium text-gray-900">Delivery method</legend>
                         <div class="mt-4 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-4">
-                            @foreach ($shipping_methods as $index => $shipping)
-
-                            @if ($index == $selected_shipping)
-                                <label class="relative bg-white ring-2 ring-indigo-500 rounded-lg shadow-sm p-4 flex cursor-pointer focus:outline-none">
-                            @else
-                                <label class="relative bg-white border border-gray-300 rounded-lg shadow-sm p-4 flex cursor-pointer focus:outline-none">
-                            @endif
-                                <input checked type="radio" name="shipping_method" value="Standard" class="sr-only" aria-labelledby="delivery_method_0_label" aria-describedby="delivery-method-0-description-0 delivery-method-0-description-1">
-                                <div class="flex-1 flex">
-                                    <div class="flex flex-col">
-                                        <span id="delivery_method_0_label" class="block text-sm font-medium text-gray-900">{{ $shipping->name }}</span>
-                                        <span id="delivery-method-0-description-0" class="mt-1 flex items-center text-sm text-gray-500">{{ $shipping->eta }}</span>
-                                        <span id="delivery-method-0-description-1" class="mt-6 text-sm font-medium text-gray-900"> {{ $currency }}{{ number_format($shipping->charges) }} </span>
+                            @foreach ($shipping_methods as $shipping)
+                                <label @click.prevent="shippingMethod = '{{ $shipping->name }}'" x-bind:class="shippingMethod == '{{ $shipping->name }}' ? 'ring-2 ring-indigo-500' : ''" class="relative bg-white border border-gray-300 rounded-lg shadow-sm p-4 flex cursor-pointer focus:outline-none">
+                                    <input x-bind:checked="shippingMethod == '{{ $shipping->name }}'" type="radio" name="shipping_method" value="{{ $shipping->name }}" class="sr-only" aria-labelledby="delivery_method_0_label" aria-describedby="delivery-method-0-description-0 delivery-method-0-description-1">
+                                    <div class="flex-1 flex">
+                                        <div class="flex flex-col">
+                                            <span class="block text-sm font-medium text-gray-900">{{ $shipping->name }}</span>
+                                            <span class="mt-1 flex items-center text-sm text-gray-500">{{ $shipping->eta }}</span>
+                                            <span class="mt-6 text-sm font-medium text-gray-900"> {{ $currency }}{{ number_format($shipping->charges) }} </span>
+                                        </div>
                                     </div>
-                                </div>
-                                
-                                @if ($index == $selected_shipping)
-                                    <svg class="h-5 w-5 text-indigo-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                                    </svg>
-                                @endif
-
-                                <div class="absolute -inset-px rounded-lg pointer-events-none" aria-hidden="true"></div>
-                            </label>
+                                        <svg x-show="shippingMethod == '{{ $shipping->name }}'" class="h-5 w-5 text-indigo-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                                        </svg>
+                                    <div class="absolute -inset-px rounded-lg pointer-events-none" aria-hidden="true"></div>
+                                </label>
                             @endforeach
                         </div>
                     </fieldset>
                 </div>
                 <!-- Payment -->
-                <div class="mt-10 border-t border-gray-200 pt-10">
+                <div x-data="{ paymentMethod: 'credit-card' }" class="mt-10 border-t border-gray-200 pt-10">
                     <h2 class="text-lg font-medium text-gray-900">Payment</h2>
                     <fieldset class="mt-4">
                         <legend class="sr-only">Payment type</legend>
                         <div id="payment-method-wrapper" class="space-y-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-10">
                             <div class="flex items-center cursor-pointer">
-                                <input checked value="Credit card" id="credit-card" name="payment_method" type="radio" class="focus:ring-indigo-500 cursor-pointer h-4 w-4 text-indigo-600 border-gray-300">
+                                <input @click="paymentMethod = 'credit-card'" checked value="Credit card" id="credit-card" name="payment_method" type="radio" class="focus:ring-indigo-500 cursor-pointer h-4 w-4 text-indigo-600 border-gray-300">
                                 <label for="credit-card" class="ml-3 block text-sm font-medium text-gray-700"> Credit card </label>
                             </div>
                             <div class="flex items-center">
-                                <input value="Bank transfer" id="bank-transfer" name="payment_method" type="radio" class="focus:ring-indigo-500 cursor-pointer h-4 w-4 text-indigo-600 border-gray-300">
+                                <input @click="paymentMethod = 'bank-transfer'" value="Bank transfer" id="bank-transfer" name="payment_method" type="radio" class="focus:ring-indigo-500 cursor-pointer h-4 w-4 text-indigo-600 border-gray-300">
                                 <label for="etransfer" class="ml-3 block text-sm font-medium text-gray-700"> Bank Transfer </label>
                             </div>
                             <div class="flex items-center cursor-pointer">
-                                <input value="Cash on delivery" id="cod" name="payment_method" type="radio" class="focus:ring-indigo-500 cursor-pointer h-4 w-4 text-indigo-600 border-gray-300">
+                                <input @click="paymentMethod = 'cash-on-delivery'" value="Cash on delivery" id="cod" name="payment_method" type="radio" class="focus:ring-indigo-500 cursor-pointer h-4 w-4 text-indigo-600 border-gray-300">
                                 <label for="credit-card" class="ml-3 block text-sm font-medium text-gray-700"> Cash On Delivery </label>
                             </div>
                         </div>
                     </fieldset>
-                    <div id="credit-card-wrapper" class="mt-6 grid grid-cols-4 gap-y-6 gap-x-4">
+                    <div x-show="paymentMethod == 'credit-card'" class="mt-6 grid grid-cols-4 gap-y-6 gap-x-4">
                         <div class="col-span-4">
                             <label for="card_number" class="block text-sm font-medium text-gray-700">Card number *</label>
                             <div class="mt-1">
@@ -143,7 +134,7 @@
                             </div>
                         </div>
                     </div>
-                    <div id="bank-transfer-wrapper" class="hidden mt-6 grid grid-cols-4 gap-y-6 gap-x-4">
+                    <div x-show="paymentMethod == 'bank-transfer'" class="mt-6 grid grid-cols-4 gap-y-6 gap-x-4">
                         <div class="col-span-4">
                             <span class="mt-4 px-4 py-2 rounded-md text-gray-700 bg-gray-200">
                                 {{ $iban ?? '--------------------------------' }}
@@ -159,7 +150,7 @@
                             </div>
                         </div>
                     </div>
-                    <div id="cod-wrapper" class="hidden mt-6 grid grid-cols-4 gap-y-6 gap-x-4">
+                    <div x-show="paymentMethod == 'cash-on-delivery'" class="mt-6 grid grid-cols-4 gap-y-6 gap-x-4">
                         <div class="col-span-4">
                             <p class="text-gray-700 mt-6">
                                 With our COD method, you can place your order online and have your items delivered to your doorstep, with payment made in full upon delivery.
@@ -189,7 +180,7 @@
                                         <p class="mt-1 text-sm text-gray-500">{{ $item->size->name }}</p>
                                     </div>
                                     <div class="ml-4 flex-shrink-0 flow-root">
-                                        <a data-item-id="{{ $item->id }}" title="Remove item from cart" class="cursor-pointer -m-2.5 bg-white p-2.5 flex items-center justify-center text-gray-400 hover:text-gray-500">
+                                        <a @click.prevent="removeItemFromCart({{ $item->id }})" title="Remove item from cart" class="cursor-pointer -m-2.5 bg-white p-2.5 flex items-center justify-center text-gray-400 hover:text-gray-500">
                                             <span class="sr-only">Remove</span>
                                             <!-- Heroicon name: solid/trash -->
                                             <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
@@ -229,23 +220,27 @@
                                         <span class="rounded-full bg-gray-200 text-xs text-gray-600 py-1 px-2 ml-2">{{ $discount_text }}</span>
                                     @endif
                                 </dt>
-                                <dd id="sub-total" class="text-sm font-medium text-gray-900">{{ $discount }}% ( -{{ $currency }}{{ number_format((($discount) / 100) * $sub_total) }} )</dd>
+                                <dd id="sub-total" class="text-sm font-medium text-gray-900">{{ $discount }}% ( -{{ $currency }}{{ number_format(($discount / 100) * $sub_total) }} )</dd>
                             </div>
                         @endif
 
                         @if ($tax > 0)
                             <div class="flex items-center justify-between">
                                 <dt class="text-sm">Tax</dt>
-                                <dd id="taxes" class="text-sm font-medium text-gray-900">{{ $tax }}% ( +{{ $currency }}{{ number_format((($tax) / 100) * $sub_total) }} )</dd>
+                                <dd id="taxes" class="text-sm font-medium text-gray-900">{{ $tax }}% ( +{{ $currency }}{{ number_format(($tax / 100) * $discounted_total) }} )</dd>
                             </div>
                         @endif
                         <div class="flex items-center justify-between">
                             <dt class="text-sm">Delivery Charges</dt>
-                            <dd id="delivery-charges" class="text-sm font-medium text-gray-900">{{ $currency }}{{ number_format($shipping_charges) }}</dd>
+                            @foreach ($shipping_methods as $shipping)
+                                <dd x-show="shippingMethod == '{{ $shipping->name }}'" class="text-sm font-medium text-gray-900">{{ $currency }}{{ number_format($shipping->charges) }}</dd>
+                            @endforeach
                         </div>
                         <div class="flex items-center justify-between border-t border-gray-200 pt-6">
                             <dt class="text-base font-medium">Total</dt>
-                            <dd id="total" class="text-base font-medium text-gray-900">{{ $currency }}{{ number_format($total) }}</dd>
+                            @foreach ($shipping_methods as $shipping)
+                                <dd x-show="shippingMethod == '{{ $shipping->name }}'" class="text-base font-medium text-gray-900">{{ $currency }}{{ number_format($total + $shipping->charges) }}</dd>
+                            @endforeach
                         </div>
                     </dl>
                     <div class="border-t border-gray-200 py-6 px-4 sm:px-6">
@@ -259,6 +254,8 @@
 @endsection
 
 @section('scripts')
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.0/dist/cdn.min.js"></script>
+
 <script>
     const handleQuantityChange = async ({ target }) => {
         const id = target.dataset.itemId;
@@ -281,42 +278,5 @@
 
         location.reload();
     };
-
-    const handleShippipngMethodChange = async () => {};
-
-    document.getElementById("item-wrapper").addEventListener("click", (e) => {
-        const path = e.composedPath();
-
-        path.map((elem) => {
-            
-            if (elem.nodeName === "A" && elem.dataset.itemId) {
-                removeItemFromCart(elem.dataset.itemId);
-            }
-        });
-    });
-
-    document.getElementById("payment-method-wrapper").addEventListener("click", (e) => {
-        const path = e.composedPath();
-
-        path.map((elem) => {
-            
-            if (elem.nodeName === "INPUT") {
-
-                if (elem.id === "bank-transfer") {
-                    document.getElementById("bank-transfer-wrapper").classList.remove("hidden");
-                    document.getElementById("credit-card-wrapper").classList.add("hidden");
-                    document.getElementById("cod-wrapper").classList.add("hidden");
-                } else if (elem.id === "cod") {
-                    document.getElementById("cod-wrapper").classList.remove("hidden");
-                    document.getElementById("credit-card-wrapper").classList.add("hidden");
-                    document.getElementById("bank-transfer-wrapper").classList.add("hidden");
-                } else {
-                    document.getElementById("credit-card-wrapper").classList.remove("hidden");
-                    document.getElementById("cod-wrapper").classList.add("hidden");
-                    document.getElementById("bank-transfer-wrapper").classList.add("hidden");
-                }
-            }
-        });
-    });
 </script>
 @endsection
