2e528af6c701437e3b8328a8e4ecf638b9067cbd
Erdum <erdumadnan@gmail.com>
2023-09-18 14:38:51 +0500

    Add convert and save uploaded image as webp method implemented

--- app/helpers.php
+++ app/helpers.php
@@ -17,3 +17,28 @@ function save_base64_to_webp($images)
     }
     return $links;
 }
+
+function save_img_to_webp($image, $extension)
+{
+    $decoded_image = null;
+    if ($extension == 'webp') {
+        $decoded_image = imagecreatefromwebp($image);
+    } elseif ($extension == 'jpeg' || $extension == 'jpg') {
+        $decoded_image = imagecreatefromjpeg($image);
+    } elseif ($extension == 'gif') {
+        $decoded_image = imagecreatefromgif($image);
+    } elseif ($extension == 'png') {
+        $decoded_image = imagecreatefrompng($image);
+    }
+    
+    if ($decoded_image == null) return response()->json(['message' => 'File type not supported'], 422);
+
+    if (!file_exists(storage_path('app/site-logos'))) {
+        mkdir(storage_path('app/site-logos'));
+    }
+    $link = 'site-logos/' . Str::random(15) . '.webp';
+    
+    imagewebp($decoded_image, storage_path('app/' . $link), 75);
+
+    return $link;
+}
