917840753517c89bcd215dfa0632a2fb7db98c4d
Erdum <erdumadnan@gmail.com>
2023-09-22 15:03:57 +0500

    Refactor home link added to the logo

--- resources/views/admin-panel/master.blade.php
+++ resources/views/admin-panel/master.blade.php
@@ -37,7 +37,9 @@
                     </button>
                 </div>
                 <div class="flex-shrink-0 flex items-center px-4">
-                    <img class="h-16 w-auto" src="{{ asset(App\Models\SiteSetting::first()->logo) }}" alt="Logo">
+                    <a href="{{ route('home') }}">
+                        <img class="h-16 w-auto" src="{{ asset(App\Models\SiteSetting::first()->logo) }}" alt="Logo">
+                    </a>
                 </div>
                 <div class="mt-5 flex-1 h-0 overflow-y-auto">
                     <nav class="px-2 space-y-1">
@@ -74,7 +76,9 @@
             <!-- Sidebar component, swap this element with another sidebar if you like -->
             <div class="flex flex-col flex-grow border-r border-gray-200 pt-5 bg-white overflow-y-auto">
                 <div class="flex items-center flex-shrink-0 px-4">
-                    <img class="h-16 w-auto" src="{{ asset(App\Models\SiteSetting::first()->logo) }}" alt="Logo">
+                    <a href="{{ route('home') }}">
+                        <img class="h-16 w-auto" src="{{ asset(App\Models\SiteSetting::first()->logo) }}" alt="Logo">
+                    </a>
                 </div>
                 <div class="mt-5 flex-grow flex flex-col">
                     <nav class="flex-1 px-2 pb-4 space-y-1">
